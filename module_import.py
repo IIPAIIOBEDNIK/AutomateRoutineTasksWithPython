8bf9921e8b046280c090d87359b306413b16b1e5
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2022-08-26 23:14:59 +0300

    Added functions and modified game

--- module_import.py
+++ module_import.py
@@ -37,7 +37,7 @@ def third():
     else:
         print('Вам не повезло. Я загадал число ' + str(secret_number))
 
-third()
+#third()
 
 
 def fourth():
@@ -48,43 +48,57 @@ def fourth():
     ties = 0
 
     while True: #Главный цикл игры
-        print('%s побед, $s поражений, $s ничьих' % (wins, losses, ties))
-    while True:
-        print('Выберите ход: (к)амень, (н)ожницы, (б)умага или (в)ыход')
-        player_move = input()
-        if player_move == 'в':
-            sys.exit() #выход из проограммы
-        if player_move == 'к' or player_move == 'н' or player_move == 'б':
-            break #выход из цикла выбора хода
-        print('Введите "к", "н", "б" или "в".')
+        print('%s побед, $s поражений, $s ничьих' % [wins, losses, ties])
+        while True:
+            print('Выберите ход: (к)амень, (н)ожницы, (б)умага или (в)ыход')
+            player_move = input()
+            if player_move == 'в':
+                sys.exit() #выход из проограммы
+            if player_move == 'к' or player_move == 'н' or player_move == 'б':
+                break #выход из цикла выбора хода
+            print('Введите "к", "н", "б" или "в".')
     #Отображение выбора пользователя:
-    if player_move == 'к':
-        print('Камень и ...')
-    if player_move == 'н':
-        print('Ножницы и ...')
-    if player_move == 'б':
-        print('Бумага и ...')
+        if player_move == 'к':
+            print('Камень и ...')
+        if player_move == 'н':
+            print('Ножницы и ...')
+        if player_move == 'б':
+            print('Бумага и ...')
     #Отображение выбора компьютера
-    random_number = random.randint(1, 3)
-    if random_number == 1:
-        computer_move = 'к'
-        print('Камень')
-    elif random_number == 2:
-        computer_move = 'н'
-        print('Ножницы')
-    elif random_number == 3:
-        computer_move = 'б'
-        print('Бумага')
+        random_number = random.randint(1, 3)
+        if random_number == 1:
+            computer_move = 'к'
+            print('Камень')
+        elif random_number == 2:
+            computer_move = 'н'
+            print('Ножницы')
+        elif random_number == 3:
+            computer_move = 'б'
+            print('Бумага')
     #Отображение и учет результата
-    if player_move == computer_move:
-        print('Ничья')
-        ties += 1
-    elif player_move == 'к' and computer_move == 'н':
-        print('Вы выиграли!')
-        wins += 1
+        if player_move == computer_move:
+            print('Ничья')
+            ties += 1
+        elif player_move == 'к' and computer_move == 'н':
+            print('Вы выиграли!')
+            wins += 1
+        elif player_move == 'б' and computer_move == 'к':
+            print('Вы выиграли!')
+            wins += 1
+        elif player_move == 'н' and computer_move == 'б':
+            print('Вы выиграли!')
+            wins += 1
+        elif player_move == 'к' and computer_move == 'б':
+            print('Вы проиграли!')
+            losses += 1
+        elif player_move == 'б' and computer_move == 'н':
+            print('Вы проиграли!')
+            losses += 1
+        elif player_move == 'н' and computer_move == 'к':
+            print('Вы проиграли!')
+            losses += 1
 
-
-
+fourth()
 
 
 
