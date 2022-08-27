cb94f5d5fa6b6b3816e6d4601d4b1bfdb4820767
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2022-08-27 13:45:47 +0300

    Added functions and functions with params

---
+++ functions.py
@@ -0,0 +1,77 @@
+def hello():
+    print('Hello!')
+    print('Hello!!!')
+    print('Привет всем.')
+
+hello()
+hello()
+hello()
+
+def hello_params(name):
+    print('Привет, ' + name)
+
+hello_params('Алиса')
+hello_params('Боб')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
